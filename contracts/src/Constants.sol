d585ad56b0504685d651a720fafa999163dc0ce3
Tihe <tihechen@gmail.com>
2023-05-16 20:03:27 +0800

    rollup contract basic setup

---
+++ contracts/src/Constants.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.8.0 <=0.9;
+
+library Constants {
+    uint256 internal constant SNARK_SCALAR_FIELD =
+        21888242871839275222246405745257275088548364400416034343698204186575808495617;
+    uint256 constant ZERO_VALUE = uint256(keccak256(abi.encodePacked("ROLLUP_LITE"))) % SNARK_SCALAR_FIELD;
+}
--- contracts/src/Rollup.sol
+++ contracts/src/Rollup.sol
@@ -1,5 +1,56 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity >0.8.0 <=0.9;
+
+import {IncrementalBinaryTree, IncrementalTreeData} from "zk-kit/incremental-merkle-tree.sol/contracts/IncrementalBinaryTree.sol";
+import {Constants} from "./Constants.sol";
 
 contract Rollup {
+    using IncrementalBinaryTree for IncrementalTreeData;
+
+    IncrementalTreeData balanceTree;
+
+    TxVerifier txVerifier;
+    WithdrawVerifier withdrawVerifier;
+
+    event Deposit(
+        uint256 balanceTreeIndex,
+        uint256 publicKeyX,
+        uint256 publicKeyY,
+        uint256 balance,
+        uint256 nonce
+    );
+
+    event Withdraw(
+        uint256 balanceTreeIndex,
+        uint256 publicKeyX,
+        uint256 publicKeyY,
+        uint256 balance,
+        uint256 nonce
+    );
+
+    event RollUp(uint256 newBalanceTreeRoot);
+
+    struct User {
+        uint256 balanceTreeLeafIndex;
+        uint256 publicKeyX;
+        uint256 publicKeyY;
+        uint256 balance;
+        uint256 nonce;
+    }
+
+    // hashedPublicKey => User
+    mapping(uint256 => User) balanceTreeUsers;
+    mapping(uint256 => bool) isPublicKeysRegistered;
+    mapping(uint256 => bool) usedNullifiers;
+
+    // index => hashedPublicKey
+    mapping(uint256 => uint256) balanceTreeKeys;
+
+    uint256 accruedFees;
+
+    constructor(TxVerifier _txVerifier, WithdrawVerifier _withdrawVerifier, uint256 _depth) {
+        txVerifier = _txVerifier;
+        withdrawVerifier = _withdrawVerifier;
+        balanceTree.initWithDefaultZeroes(_depth);
+    }
 }
