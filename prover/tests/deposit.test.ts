a2431879267d5a875d25b3271dac83b59aeea02b
Tihe <tihechen@gmail.com>
2023-06-12 18:29:02 +0800

    deposit unit test done

--- prover/tests/deposit.test.ts
+++ prover/tests/deposit.test.ts
@@ -1,16 +1,16 @@
 import { genPrivKey } from "maci-crypto"
-import { genPublicKey, stringifyBigInts, arrayifySignature, insertTree, fillSiblings } from "./utils/helpers"
+import { genPublicKey, stringifyBigInts, fillSiblings } from "./utils/helpers"
 import path from "path"
-const { eddsa, smt, poseidon } = require("circomlibjs");
+const { smt, poseidon } = require("circomlibjs");
 const wasm_tester = require("circom_tester").wasm
 
-describe("withdraw.circom", () => {
-    it("Withdraw()", async () => {
+describe("deposit.circom", () => {
+    it("Deposit()", async () => {
         // Compile the circuit
         const circuit = await wasm_tester(
-            path.join(__dirname, "../circuits", "withdraw.circom"),
+            path.join(__dirname, "../circuits", "deposit.circom"),
             {
-                output: path.join(__dirname, "../build", "withdraw"),
+                output: path.join(__dirname, "../build", "deposit"),
                 recompile: true,
                 reduceConstraints: false
             }
@@ -18,40 +18,31 @@ describe("withdraw.circom", () => {
 
         const depth = 6;
 
-        // Create the mock user & nullifier
+        // Create the mock user
         const privA = genPrivKey().toString()
         const pubA = genPublicKey(privA)
 
         const balanceTree = await smt.newMemEmptyTrie()
+
         const leaf = poseidon([pubA[0], pubA[1], BigInt(1e18), 0])
 
-        await balanceTree.insert(0, leaf)
-
-        const newLeaf = poseidon([pubA[0], pubA[1], BigInt(0.5e18), 0])
-        const res = await balanceTree.update(0, newLeaf)
+        const res = await balanceTree.insert(0, leaf)
 
         const siblings = fillSiblings(depth, res.siblings)
-
-        const nullifier = genPrivKey()
-
-        // Sign the nullifier
-        const signature = arrayifySignature(eddsa.signMiMC(privA, nullifier))
 
         // Create circuit inputs
         // All input format must be string
         const circuitInputs = stringifyBigInts({
-            publicKey: pubA,
-            signature,
-            nullifier,
             balanceTreeRoot: res.oldRoot,
-            balance: BigInt(0.5e18),
+            publicKey: pubA,
+            balance: BigInt(1e18),
             nonce: 0,
             pathElements: siblings,
             oldKey: res.isOld0 ? 0 : res.oldKey,
             oldValue: res.isOld0 ? 0 : res.oldValue,
             isOld0: res.isOld0 ? 1 : 0,
-            newKey: res.newKey,
-            func: [0, 1]
+            newKey: res.oldKey,
+            func: [1, 0]
         })
 
         const witness = await circuit.calculateWitness(circuitInputs)
