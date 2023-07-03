ffe1fb34edc952f885269aa6a0854cfb62d19ec1
Tihe <tihechen@gmail.com>
2023-07-03 16:10:04 +0800

    add event handlers to update merkle tree

--- operator/database/merkletree.go
+++ operator/database/merkletree.go
@@ -3,7 +3,11 @@ package database
 import (
 	"context"
 	"errors"
+	"math/big"
 
+	"github.com/chentihe/zk-rollup-lite/operator/models"
+	"github.com/chentihe/zk-rollup-lite/operator/txmanager"
+	"github.com/iden3/go-iden3-crypto/poseidon"
 	sql "github.com/iden3/go-merkletree-sql/db/pgx/v5"
 	"github.com/iden3/go-merkletree-sql/v2"
 
@@ -33,4 +37,23 @@ func InitMerkleTree() (*merkletree.MerkleTree, error) {
 	}
 
 	return mt, nil
+}
+
+func GenerateAccountLeaf(account *models.AccountModel) (*big.Int, error) {
+	publicKey, err := txmanager.DecodePublicKeyFromString(account.PublicKey)
+	if err != nil {
+		return nil, err
+	}
+
+	hashedLeaf, err := poseidon.Hash([]*big.Int{
+		publicKey.X,
+		publicKey.Y,
+		account.Balance,
+		big.NewInt(account.Nonce),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return hashedLeaf, nil
 }
